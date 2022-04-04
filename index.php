d54395af49dd923ba590897797bafeaec64e8f28
lai2-01 <lai2-01@itismeucci.com>
2022-04-04 11:36:06 +0200

    readsdme

--- index.php
+++ index.php
@@ -7,15 +7,14 @@
     header("Access-Control-Max-Age: 3600");
     header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-
     $data = ['id' => 10001, 'birthDate' => "1953-09-01", 'firstName' => 'Georgi', 'lastName' => 'Facello', 'gender' => 'M', 'hireDate' => '1986-06-25'];
     
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         echo json_encode($data);
         $sql = "SELECT * from employees";
         $result = $conn->query($sql);
-        echo ($result);
-        return $result;
+        // echo ($result);
+        // return $result;
     }else 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo json_encode($data);
