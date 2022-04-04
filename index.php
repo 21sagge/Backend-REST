ecaf19b4097234b2edafa518810b371d1fc10f00
lai2-01 <lai2-01@itismeucci.com>
2022-04-04 11:31:46 +0200

    connessione db

--- index.php
+++ index.php
@@ -1,11 +1,5 @@
 <?php
-    $servername = "172.17.0.1:3306";
-    $user = "root";
-    $pass = "pise";
-    $db="mydb";
-
-    // Create connection
-    $conn = mysqli_connect($servername, $user, $pass, $db) or die("Connessione non riuscita". mysqli_connect_error());
+    $conn = mysqli_connect("172.17.0.1:3306", "root", "pw", "mydb") or die("Connessione non riuscita: ". mysqli_connect_error());
 
     header("Access-Control-Allow-Origin: *");
     header("Content-Type: application/json; charset=UTF-8");
@@ -15,8 +9,8 @@
 
 
     $data = ['id' => 10001, 'birthDate' => "1953-09-01", 'firstName' => 'Georgi', 'lastName' => 'Facello', 'gender' => 'M', 'hireDate' => '1986-06-25'];
+    
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        // The request is using the GET method
         echo json_encode($data);
         $sql = "SELECT * from employees";
         $result = $conn->query($sql);
@@ -24,15 +18,11 @@
         return $result;
     }else 
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        // The request is using the POST method
         echo json_encode($data);
     }else 
     if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
-        // The request is using the DELETE method
         echo json_encode($data);
     }else 
     if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
-        // The request is using the PUT method
         echo json_encode($data);
     }
-?>
